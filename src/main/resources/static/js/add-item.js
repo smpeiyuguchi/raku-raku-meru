cbe1d4f42f6e3bc9acee90e8bf487afa0cdcbd87
shumpeiyuguchi <shumpei.yuguchi@rakus-partners.co.jp>
2020-04-03 11:44:46 +0900

    商品追加用のHTMLを実装

---
+++ src/main/resources/static/js/add-item.js
@@ -0,0 +1,57 @@
+$(function(){
+	$("#parent").on("change", function(){
+		var hostUrl = "http://localhost:8080/search_form_api/refine_category";
+		var parentIdParam = $("#parent").val();
+		var childSelect = $("#child").children();
+		$.ajax({
+			url : hostUrl,
+			type : 'POST',
+			dataType : 'json',
+			data : {
+				parentId : parentIdParam
+			},
+			async : true
+		}).done(function(data){
+			console.log(data);
+			console.dir(JSON.stringify(data));
+			$('#child > option:last-child').remove();
+			var childOption = data.childCategoryList;
+			for(var i = 0; i < childOption.length; i++){
+				$('#child').append($('<option>').html(childOption[i].name).val(childOption[i].id));
+			}
+		}).fail(function(XMLHttpRequest, textStatus, errorThrown){
+			alert("エラーが発生しました！");
+			console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+			console.log("textStatus     : " + textStatus);
+			console.log("errorThrown    : " + errorThrown.message);
+		});
+	});
+	
+	$("#child").on("change", function(){
+		var hostUrl = "http://localhost:8080/search_form_api/refine_grand_child_category";
+		var childIdParam = $("#child").val();
+		var childSelect = $("#grandChild").children();
+		$.ajax({
+			url : hostUrl,
+			type : 'POST',
+			dataType : 'json',
+			data : {
+				childId : childIdParam
+			},
+			async : true
+		}).done(function(data) {
+			console.log(data);
+			console.dir(JSON.stringify(data));
+			$('#grandChild > option:last-child').remove();
+			var grandChildOption = data.grandChildCategoryList;
+			for(var i = 0; i < grandChildOption.length; i++){
+				$('#grandChild').append($('<option>').html(grandChildOption[i].name).val(grandChildOption[i].id));
+			}				
+		}).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+			alert("エラーが発生しました！");
+			console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+			console.log("textStatus     : " + textStatus);
+			console.log("errorThrown    : " + errorThrown.message);
+		});
+	});
+});
